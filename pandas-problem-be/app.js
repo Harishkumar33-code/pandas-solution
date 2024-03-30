c8128ccaab3a0ad95db717f9923392894bb15394
Harishkumar T <151904162+Harishkumar33-code@users.noreply.github.com>
2024-03-31 01:48:21 +0530

    Update app.js

--- pandas-problem-be/app.js
+++ pandas-problem-be/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const port = process.env.PORT || 3001;
-const connection = require('./db');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
@@ -11,28 +10,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', router);
 
-router.post('/login', (req, res) => {
-const loginQuery = `SELECT * FROM tbl_users WHERE email = ?`;
-const loginParams = [req.body.email];
+// Import routes
+const userRoutes = require('./routes/userRoutes');
 
-  connection.query(loginQuery,loginParams, (err, results) => {
-    if (err) {
-      console.error('Error querying database:', err);
-      res.status(500).send('Error querying database');
-      return;
-    } else {
-      if (results.length === 0) {
-        res.status(401).send('Invalid username');
-        return;
-      } else {
-        res.status(200).send('Valid username');
-        return
-      }
-    }
-  });
-});
+// Routes
+router.use('/user', userRoutes);
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port http://localhost:${port}/`);
 });
